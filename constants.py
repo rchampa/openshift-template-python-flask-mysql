ee926a3f2f644df0d94b2185ff50f4a1b9b85f5b
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-27 20:10:45 +0100

    setting ddbb

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 import os
 #MYSQL_URI = 'mysql://'+os.environ['OPENSHIFT_MYSQL_DB_USERNAME']+':'+os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']+"@"+os.environ['OPENSHIFT_MYSQL_DB_HOST']+':'+os.environ['OPENSHIFT_MYSQL_DB_PORT']
-MYSQL_URI = os.environ['OPENSHIFT_MYSQL_DB_URL']
+MYSQL_URI = os.environ['OPENSHIFT_MYSQL_DB_URL']+os.environ['OPENSHIFT_APP_NAME']
 #MYSQL_URI = 'mysql://root:@localhost/scheduler'
